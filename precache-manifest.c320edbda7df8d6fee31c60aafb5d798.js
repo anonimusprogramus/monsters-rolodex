5ebbae378cbc32fba5225e7ab1f0e667bd947b96
Anonimus Programus <anonimus.programus@gmail.com>
2019-10-01 20:27:30 +0700

    Updates

---
+++ precache-manifest.c320edbda7df8d6fee31c60aafb5d798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcd2793f98b714514895be9ab6d5b34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c9c3030003eb7df31d51",
+    "url": "/monsters-rolodex/static/css/main.05c60bb0.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "c9c3030003eb7df31d51",
+    "url": "/monsters-rolodex/static/js/main.80acd376.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
